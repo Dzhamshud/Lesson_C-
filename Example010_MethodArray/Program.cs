02f95d19662a4afaa8e767ab802c728d4ab09403
Pavel.Koshelev <koshelev_p@mail.ru>
2022-07-27 23:08:09 +0300

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 4, 12, 65, 12, 8, 3, 84, 14 };
+
+int n=array.Length;
+int find=12;
+int index=0;
+
+while(index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;//остановить проверку при первом совпадении
+    }
+
+    index++;
+}
